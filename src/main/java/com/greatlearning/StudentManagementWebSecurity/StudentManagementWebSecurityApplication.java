edf340db40c67b1dceea0b79b51b6607c8ac3858
vaibhavkoshta21 <101541709+vaibhavkoshta21@users.noreply.github.com>
2022-11-09 09:35:15 -0800

    Create StudentManagementWebSecurityApplication.java

---
+++ src/main/java/com/greatlearning/StudentManagementWebSecurity/StudentManagementWebSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.greatlearning.StudentManagementWebSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementWebSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementWebSecurityApplication.class, args);
+	}
+
+}
