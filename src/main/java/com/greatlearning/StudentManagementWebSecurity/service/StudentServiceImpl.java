0ed1f12ce9648d229f6d9ef4db35cd6b8bfb6770
vaibhavkoshta21 <101541709+vaibhavkoshta21@users.noreply.github.com>
2022-11-09 09:33:18 -0800

    Create StudentServiceImpl.java

---
+++ src/main/java/com/greatlearning/StudentManagementWebSecurity/service/StudentServiceImpl.java
@@ -0,0 +1,52 @@
+package com.greatlearning.StudentManagementWebSecurity.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.greatlearning.StudentManagementWebSecurity.entity.Student;
+import com.greatlearning.StudentManagementWebSecurity.repository.StudentRepository;
+
+@Repository
+public class StudentServiceImpl implements StudentService  {
+	
+	@Autowired
+	StudentRepository studentRepository;
+
+	@Transactional
+	public List<Student> findAll() {
+
+		List<Student> students = studentRepository.findAll();
+
+		return students;
+	}
+
+	@Transactional
+	public Student findById(int id) {
+
+		Student student = new Student();
+
+		// find record with Id from the database table
+		student = studentRepository.findById(id).get();
+
+		return student;
+	}
+
+	@Transactional
+	public void save(Student theStudent) {
+
+		studentRepository.save(theStudent);
+
+	}
+
+	@Transactional
+	public void deleteById(int id) {
+
+		studentRepository.deleteById(id);
+
+	}
+
+}
