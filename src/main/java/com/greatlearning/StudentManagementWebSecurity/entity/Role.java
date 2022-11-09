51ac43e39c15097e107b653f5678144a69939734
vaibhavkoshta21 <101541709+vaibhavkoshta21@users.noreply.github.com>
2022-11-09 09:27:21 -0800

    Create Role.java

---
+++ src/main/java/com/greatlearning/StudentManagementWebSecurity/entity/Role.java
@@ -0,0 +1,29 @@
+package com.greatlearning.StudentManagementWebSecurity.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Data
+@Getter
+@Setter
+public class Role {
+	
+	@Id
+	@Column(name = "role_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+}
